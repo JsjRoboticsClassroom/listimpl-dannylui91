32156fb5746e5c63c85979d4f3efd0b578e274a0
dannylui91 <dannylui91@gmail.com>
2016-09-07 21:47:49 -0400

    changes to main

--- src/nyc/c4q/ashiquechowdhury/Main.java
+++ src/nyc/c4q/ashiquechowdhury/Main.java
@@ -8,6 +8,12 @@ public class Main {
     public static void main(String[] args) {
         AbstractList list = new OrderedList(50);
         list.add(300);
+
+        list.set(0, 1000);
+        list.set(1, 2000);
+        //for some reason your .add method does list.set(2, 300)
+        list.set(3, 3000);
+
         System.out.println(list.get(2));
     }
 }
